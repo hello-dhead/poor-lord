bef8692b26dd5bed055f550cb1ac70337ea16ecd
dhead <whguddn6048@gmail.com>
2021-11-25 00:45:53 +0900

    버프 Vulture : 적이 죽을때마다 공격력 증가 추가

---
+++ Assets/Scripts/Buff/BuffVulture.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace poorlord
+{
+    /// <summary>
+    /// 적이 죽을때마다 공격력 증가
+    /// </summary>
+    public class BuffVulture : ContinuousBuff
+    {
+        public override string BuffName { get { return "Vulture"; } protected set {} }
+
+        public override void Init(Unit bufftarget)
+        {
+            Target = bufftarget;
+            GameManager.Instance.MessageSystem.Subscribe(typeof(MonsterDeadEvent), this);
+        }
+
+        public override Buff Copy()
+        {
+            return new BuffVulture();
+        }
+
+        public override void Dispose()
+        {
+            GameManager.Instance.MessageSystem.Unsubscribe(typeof(MonsterDeadEvent), this);
+            Target = null;
+        }
+
+        public override bool OnEvent(IEvent e)
+        {
+            if (e.GetType() == typeof(MonsterDeadEvent))
+            {
+                GameManager.Instance.EffectSystem.CreateEffect("HealExplosionGreen", Target.UnitPosition + new Vector3(0, 0.4f, -0.1f), new Vector3(0.5f, 0.5f, 0.5f), Quaternion.Euler(new Vector3(-90, 0, 0)), 1);
+                int hp = Target.HP;
+                if (hp + (int)(hp * 0.1) < Target.MaxHP)
+                {
+                    Target.SetHP(hp + (int)(hp * 0.1));
+                }
+                else
+                {
+                    Target.SetHP(Target.MaxHP);
+                }
+            }
+            return true;
+        }
+    }
+}
